fd60c9d01af8c6875200c94ba680ecb2ce8e7137
claudere <129028339+claudere0@users.noreply.github.com>
2026-07-28 19:42:40 +0500

    scripts/constants.py separated from game.py

---
+++ scripts/constants.py
@@ -0,0 +1,104 @@
+import pygame
+from enum import Enum, auto
+
+pygame.mixer.pre_init(44100,-16,2,512)
+pygame.init()
+
+FPS = 60
+FONT = pygame.font.SysFont('monospace', 36)
+
+CELL_SIZE = 32
+GRID_WIDTH = 16
+GRID_HEIGHT = 12
+
+TOP_PANEL_HEIGHT = 64
+BOTTOM_PANEL_HEIGHT = 64
+
+WIDTH = GRID_WIDTH * CELL_SIZE
+HEIGHT = TOP_PANEL_HEIGHT + (GRID_HEIGHT * CELL_SIZE) + BOTTOM_PANEL_HEIGHT
+
+THEMES = [
+    {
+        "name": "CLASSIC",
+        "bg": (0, 0, 0),
+        "snake": (0, 255, 0),
+        "food_common": (255, 0, 0),
+        "food_rare": (255, 255, 0),
+        "ui_text": (255, 255, 255),
+        "ui_bg": (0, 0, 0)
+    },
+    {
+        "name": "MONOCHROME",
+        "bg": (0, 0, 0),
+        "snake": (255, 255, 255),
+        "food_common": (0, 255, 0),
+        "food_rare": (255, 0, 0),
+        "ui_text": (0, 255, 0),
+        "ui_bg": (0, 0, 0)
+    },
+    {
+        "name": "CYBERPUNK",
+        "bg": (0, 0, 255),
+        "snake": (255, 255, 0),
+        "food_common": (255, 0, 255),
+        "food_rare": (0, 255, 255),
+        "ui_text": (255, 255, 255),
+        "ui_bg": (255, 0, 255)
+    },
+    {
+        "name": "ROYAL",
+        "bg": (0, 0, 255),
+        "snake": (255, 255, 255),
+        "food_common": (255, 255, 0),
+        "food_rare": (255, 0, 255),
+        "ui_text": (255, 255, 255),
+        "ui_bg": (0, 0, 255)
+    },
+    {
+        "name": "AQUA WAVE",
+        "bg": (0, 255, 255),
+        "snake": (0, 0, 255),
+        "food_common": (255, 0, 0),
+        "food_rare": (255, 255, 0),
+        "ui_text": (0, 0, 0),
+        "ui_bg": (255, 255, 255)
+    },
+    {
+        "name": "ICE",
+        "bg": (255, 255, 255),
+        "snake": (0, 255, 255),
+        "food_common": (0, 0, 255),
+        "food_rare": (255, 0, 255),
+        "ui_text": (0, 0, 255),
+        "ui_bg": (255, 255, 255)
+    },
+    {
+        "name": "TAXI",
+        "bg": (255, 255, 0),
+        "snake": (0, 0, 0),
+        "food_common": (255, 0, 0),
+        "food_rare": (255, 0, 255),
+        "ui_text": (0, 0, 0),
+        "ui_bg": (255, 255, 0)
+    },
+    {
+        "name": "VOLCANO",
+        "bg": (255, 0, 0),
+        "snake": (0, 0, 0),
+        "food_common": (255, 255, 0),
+        "food_rare": (255, 255, 255),
+        "ui_text": (255, 255, 0),
+        "ui_bg": (0, 0, 0)
+    }
+]
+
+CONFIG_FILE = "config.json"
+
+THEME_SPRITESHEET_SIZE = 128
+SCORE_OFFSET_X = 16
+
+class StateID(Enum):
+    MENU = auto()
+    PLAYING = auto()
+    GAME_OVER = auto()
+    SETTINGS = auto()
--- scripts/game.py
+++ scripts/game.py
@@ -1,104 +1,7 @@
-import pygame
 import json, os
 from random import randint
 from pygame.math import Vector2
-from enum import Enum, auto
-pygame.mixer.pre_init(44100,-16,2,512)
-pygame.init()
-
-FPS = 60
-FONT = pygame.font.SysFont('monospace', 36)
-
-CELL_SIZE = 32
-GRID_WIDTH = 16
-GRID_HEIGHT = 12
-
-TOP_PANEL_HEIGHT = 64
-BOTTOM_PANEL_HEIGHT = 64
-
-WIDTH = GRID_WIDTH * CELL_SIZE
-HEIGHT = TOP_PANEL_HEIGHT + (GRID_HEIGHT * CELL_SIZE) + BOTTOM_PANEL_HEIGHT
-
-THEMES = [
-    {
-        "name": "CLASSIC",
-        "bg": (0, 0, 0),
-        "snake": (0, 255, 0),
-        "food_common": (255, 0, 0),
-        "food_rare": (255, 255, 0),
-        "ui_text": (255, 255, 255),
-        "ui_bg": (0, 0, 0)
-    },
-    {
-        "name": "MONOCHROME",
-        "bg": (0, 0, 0),
-        "snake": (255, 255, 255),
-        "food_common": (0, 255, 0),
-        "food_rare": (255, 0, 0),
-        "ui_text": (0, 255, 0),
-        "ui_bg": (0, 0, 0)
-    },
-    {
-        "name": "CYBERPUNK",
-        "bg": (0, 0, 255),
-        "snake": (255, 255, 0),
-        "food_common": (255, 0, 255),
-        "food_rare": (0, 255, 255),
-        "ui_text": (255, 255, 255),
-        "ui_bg": (255, 0, 255)
-    },
-    {
-        "name": "ROYAL",
-        "bg": (0, 0, 255),
-        "snake": (255, 255, 255),
-        "food_common": (255, 255, 0),
-        "food_rare": (255, 0, 255),
-        "ui_text": (255, 255, 255),
-        "ui_bg": (0, 0, 255)
-    },
-    {
-        "name": "AQUA WAVE",
-        "bg": (0, 255, 255),
-        "snake": (0, 0, 255),
-        "food_common": (255, 0, 0),
-        "food_rare": (255, 255, 0),
-        "ui_text": (0, 0, 0),
-        "ui_bg": (255, 255, 255)
-    },
-    {
-        "name": "ICE",
-        "bg": (255, 255, 255),
-        "snake": (0, 255, 255),
-        "food_common": (0, 0, 255),
-        "food_rare": (255, 0, 255),
-        "ui_text": (0, 0, 255),
-        "ui_bg": (255, 255, 255)
-    },
-    {
-        "name": "TAXI",
-        "bg": (255, 255, 0),
-        "snake": (0, 0, 0),
-        "food_common": (255, 0, 0),
-        "food_rare": (255, 0, 255),
-        "ui_text": (0, 0, 0),
-        "ui_bg": (255, 255, 0)
-    },
-    {
-        "name": "VOLCANO",
-        "bg": (255, 0, 0),
-        "snake": (0, 0, 0),
-        "food_common": (255, 255, 0),
-        "food_rare": (255, 255, 255),
-        "ui_text": (255, 255, 0),
-        "ui_bg": (0, 0, 0)
-    }
-]
-
-CONFIG_FILE = "config.json"
-
-THEME_SPRITESHEET_SIZE = 128
-SCORE_OFFSET_X = 16
-
+from .constants import *
 
 def load_config():
     default_config = {"theme_index": 0, "high_score": 0, "graphics_mode": "MINIMAL", "volume": 2}
@@ -123,12 +26,6 @@ def save_config(theme_index, high_score, graphics_mode, volume):
     }
     with open(CONFIG_FILE, "w") as f:
         json.dump(data, f, indent=4)
-
-class StateID(Enum):
-    MENU = auto()
-    PLAYING = auto()
-    GAME_OVER = auto()
-    SETTINGS = auto()
 
 class MenuState:
     def __init__(self, game):
